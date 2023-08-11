57aa6a6c955b228ec5399c9f46ab649d9e9e7223
Mark <ajineer@outlook.com>
2023-08-11 14:23:37 -0600

    refactored back functionality in my menus, refactored loop logic

--- lib/db/cli.py
+++ lib/db/cli.py
@@ -27,8 +27,6 @@ class Menu:
         answers = inquirer.prompt(self.questions)
         self.menu_dict.get(answers['functions'])()
         
-
-
 class app:
 
     def __init__(self):
@@ -44,7 +42,7 @@ class app:
             self.make_user.__name__: self.make_user, 
             self.fetch_user.__name__: self.fetch_user, 
             self.delete_user.__name__: self.delete_user, 
-            self.back.__name__: self.back})
+            self.exit.__name__: self.exit})
         
         self.user_menu = Menu(
             {self.select_list.__name__: self.select_list, 
@@ -64,6 +62,14 @@ class app:
             self.change_status.__name__: self.change_status, 
             self.change_task_description.__name__: self.change_task_description, 
             self.back.__name__: self.back})
+    
+    def save_to_db(self, save_object):
+        session.add(save_object)
+        session.commit()
+
+    def delete_from_db(self, delete_obj):
+        session.delete(delete_obj)
+        session.commit()
 
     def run_app(self):
 
@@ -72,66 +78,88 @@ class app:
     def back(self):
 
         return_functions = {
-            1: self.print_user,
-            2: self.print_list,
-            3: self.print_task
+            1: self.main_loop,
+            2: self.user_loop,
+            3: self.list_loop,
+            4: self.task_loop
             }
-        if Menu.currentIndex != 0:
-            print(Menu.currentIndex)
-            return_functions.get(Menu.currentIndex)()
-            Menu.all[Menu.currentIndex-1].loop()
-        else:
-            print("good bye!")
-            pass
+    
+        return_functions.get(Menu.currentIndex)()
+        
 
-    def print_user(self):
+    def print_console(self, table):
+        
         console = Console()
-        console.print(self.selected_user.__repr__())
+        console.print(table)
 
-    def print_list(self):
-        console = Console()
-        console.print(self.selected_list.__repr__())
+    def exit(self):
+        
+        return 1
+    
+    def main_loop(self):
 
-    def print_task(self):
-        console = Console()
-        console.print(self.selected_task.__repr__())
+        self.main_menu.loop()
+
+    def user_loop(self):
+
+        self.print_console(self.selected_user.__repr__())
+        self.user_menu.loop()
+
+    def list_loop(self):
+        
+        self.print_console(self.selected_list.__repr__())
+        self.list_menu.loop()
+
+    def task_loop(self):
+
+        self.print_console(self.selected_task.__repr__())
+        self.task_menu.loop()
 
     def fetch_user(self):
 
         f_name = input("Enter first name: ")
         l_name = input("Enter last name: ")
         self.selected_user = session.query(User).filter(User.first_name == f_name and User.last_name == l_name).first()
+
         if (self.selected_user):
-            self.print_user()
-            self.user_menu.loop()
+            
+            self.user_loop()
+        
         else:
+
             print("User not found.")
             self.main_menu.loop()
     
     def select_list(self):
 
-        self.print_user()
+        self.print_console(self.selected_user.__repr__())
         list_name = input("Select List: ")
         self.selected_list = session.query(List).filter(List.name == list_name).first()
+
         if self.selected_list:
-            self.print_list()
-            self.list_menu.loop()
+            
+            self.list_loop()
+        
         else:
+
             print("List not found")
             self.user_menu.loop()
 
     def select_task(self):
 
-        self.print_list()
+        self.print_console(self.selected_user.__repr__())
 
         task_description = input("Select Task: ")
         self.selected_task = session.query(Task).filter(Task.description == task_description).first()
+        
         if self.selected_task:
-            self.print_task()
-            self.task_menu.loop()
+
+            self.task_loop()
+        
         else:
+
             print("Task not found")
-            self.list_menu.loop()
+            self.list_loop()
 
     def make_user(self):
 
@@ -139,35 +167,45 @@ class app:
         l_name = input("Enter last name: ")
 
         new_user = User(first_name = f_name, last_name = l_name)
-        session.add(new_user)
-        session.commit()
+        self.save_to_db(new_user)
         self.selected_user = new_user
+
         if self.selected_user:
-            self.user_menu.loop()
+            self.user_loop()
     
     def make_list(self):
         
         list_name = input("Enter List name: ")
         new_list = List(name = list_name, user_id = self.selected_user.id)
-        session.add(new_list)
-        session.commit()
-        self.selected_list = new_list
-        if self.selected_list:
-            self.list_menu.loop()
+
+        if new_list:
+
+            self.save_to_db(new_list)
+            self.selected_list = new_list
+            self.list_loop()
 
     def make_category(self):
         
         new_title = input("Enter input: ")
-        new_cat = Category(title=new_title)
-        session.add(new_cat)
-        session.commit()
-        self.list_menu.loop()
+
+        if new_title:
+            
+            new_cat = Category(title=new_title)
+            self.save_to_db(new_cat)
+
+        else:
+
+            print("Category can't be empty!")
+        
+        self.list_loop()
+
 
     def make_task(self):
 
         task_description = input("Enter task: ")
 
         if task_description:
+
             new_task = Task(description = task_description, list_id = self.selected_list.id)
             categories = session.query(Category).all()
         
@@ -175,15 +213,13 @@ class app:
             answers = inquirer.prompt(cat_list)
             self.selected_category = answers["Categories"]
             new_task.categories = session.query(Category).filter(Category.title.in_(answers["Categories"])).all()
-            session.add(new_task)
-            session.commit()
+            self.save_to_db(new_task)
 
         else:
+
             print("Task description must be non-empty string!")
         
-        self.print_list()
-        self.list_menu.loop()
-
+        self.list_loop()
 
     def delete_user(self):
         
@@ -191,41 +227,39 @@ class app:
         user_id = int(input("Enter user id to be deleted: "))
         
         if f_name and user_id and type(user_id) == int:
+
             delete_user = session.query(User).filter(User.id == user_id and User.first_name == f_name).first()
-            session.delete(delete_user)
-            session.commit()
+            self.delete_from_db(delete_user)
 
         else: 
+
             print("Invalid name or id.")
         
         self.user_menu.loop()
 
-        
     def delete_list(self):
 
-        self.print_list()
+        self.print_console(self.selected_user.__repr__())
         list_name = input("Enter list to be deleted: ")
         list_id = int(input("Enter list id: "))
         delete_list = session.query(List).filter(List.name == list_name and List.id == list_id).first()
-        session.delete(delete_list)
-        session.commit()
-        self.print_list()
-        self.list_menu.loop()
+        self.delete_from_db(delete_list)
+        self.user_loop()
 
     def delete_task(self):
 
-        console = Console()
-        console.print(self.selected_list.__repr__())
+        self.print_console(self.selected_list.__repr__())
 
         task_id = int(input("Enter task id to be deleted: "))
 
         if task_id:
             delete_task = session.query(Task).filter(Task.id == task_id).first()
-            session.delete(delete_task)
-            session.commit()
+            self.delete_from_db(delete_task)
 
-        self.print_list()
-        self.list_menu.loop()
+        else:
+            print("Invalid task description!")
+
+        self.list_loop()
 
     def change_status(self):
         
@@ -234,32 +268,32 @@ class app:
         fetch_task = session.query(Task).filter(Task.id == self.selected_task.id).first()
         fetch_task.complete = 0 if answer["choices"] == "Incomplete" else 1
         session.commit() 
-        self.print_task()
-        self.task_menu.loop()
+        self.task_loop()
 
     def change_task_description(self):
-        self.print_task()
+        
+        self.print_console(self.selected_task.__repr__())
         new_description = input("Enter new description: ")
         fetch_task = session.query(Task).filter(Task.id == self.selected_task.id).first()
         fetch_task.description = new_description
         session.commit()
         self.selected_task = session.query(Task).filter(Task.id == self.selected_task.id).first()
-        self.print_task()
-        self.task_menu.loop()
+        self.task_loop()
 
     def show_by_category(self):
+        
         choices = [inquirer.List("choices", message="Select a Category", choices=[cat.title for cat in session.query(Category).all()])]
         answer = inquirer.prompt(choices)
         tasks = session.query(Task).filter(Task.list_id == self.selected_list.id and Task.categories.in_(answer["choices"]))
-        console = Console()
         task_table = TB(title = "Task by Category")
         task_table.add_column("id")
         task_table.add_column("Category")
         task_table.add_column("Task")
+        
         for task in tasks:
             task_table.add_row(f"{task.id}", answer["choices"], task.description)
-        console.print(task_table)
-        self.task_menu.loop()
+        self.print_console(task_table)
+        self.task_loop()
 
 if __name__ == '__main__':
 
